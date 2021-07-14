63f115dbf8dac5fe97dd50dda11b6bdf7b3bd5a5
kieran <kieran@0x51d.fun>
2021-07-14 20:59:48 +0100

    :tada: More Setup

---
+++ src/Providers/DocketryServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Docketry\Docketry\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class DocketryServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+}
